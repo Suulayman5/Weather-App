17fa1ac78ea5c8aac88fca0897fb7f157c512e12
ajah suleiman <ajahsuleiman@gmail.com>
2024-06-19 16:57:04 +0100

    added error message

--- index.js
+++ index.js
@@ -7,30 +7,40 @@ const weatherIcon = document.querySelector('.weather-icon')
 
 async function checkWeather(city){
     const reponse = await fetch(apiUrl + city + `&appid=${apiKey}`)
-    let data = await reponse.json()
-    console.log(data)
 
-    document.querySelector('.City').innerHTML = data.name
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
-    document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
-
-    if (data.weather[0].main === 'Clouds') {
-        weatherIcon.src = 'images/cloudy_164806.png'
-    }else if (data.weather[0].main === 'Clear') {
-        weatherIcon.src = 'images/sun_3104611.png'
-    }else if (data.weather[0].main === 'Drizzle') {
-        weatherIcon.src = 'images/drizzle.png'
-    }else if (data.weather[0].main === 'Rain') {
-        weatherIcon.src = 'images/rain.png'
-    }else if (data.weather[0].main === 'Mist') {
-        weatherIcon.src = 'images/mist_13882572.png'
+    if (reponse.status == 404) {
+        document.querySelector('.error').style.display = 'block'
+        document.querySelector('.weather').style.display = 'none'
+    }else{
+        let data = await reponse.json()
+        console.log(data)
+    
+        document.querySelector('.City').innerHTML = data.name
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+        document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
+    
+        if (data.weather[0].main === 'Clouds') {
+            weatherIcon.src = 'images/cloudy_164806.png'
+        }else if (data.weather[0].main === 'Clear') {
+            weatherIcon.src = 'images/sun_3104611.png'
+        }else if (data.weather[0].main === 'Drizzle') {
+            weatherIcon.src = 'images/drizzle.png'
+        }else if (data.weather[0].main === 'Rain') {
+            weatherIcon.src = 'images/rain.png'
+        }else if (data.weather[0].main === 'Mist') {
+            weatherIcon.src = 'images/mist_13882572.png'
+        }
+    
+        document.querySelector('.weather').style.display = 'block'
+        document.querySelector('.error').style.display = 'none'
     }
-}
+    }
+    searchBtn.addEventListener('click', () =>{
+        checkWeather(searchBox.value)
+    })
+    
 
-searchBtn.addEventListener('click', () =>{
-    checkWeather(searchBox.value)
-})
 
 
 // checkWeather()
