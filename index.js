e37c37b01aee7480f38d097c75f6c82b3abed694
ajah suleiman <ajahsuleiman@gmail.com>
2024-06-19 16:40:07 +0100

    added diff img for diff weather conditions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
 const searchBox = document.querySelector('.search input')
 const searchBtn = document.querySelector('.search Button')
+const weatherIcon = document.querySelector('.weather-icon')
+
 async function checkWeather(city){
     const reponse = await fetch(apiUrl + city + `&appid=${apiKey}`)
     let data = await reponse.json()
@@ -12,6 +14,18 @@ async function checkWeather(city){
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
     document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
+
+    if (data.weather[0].main === 'Clouds') {
+        weatherIcon.src = 'images/cloudy_164806.png'
+    }else if (data.weather[0].main === 'Clear') {
+        weatherIcon.src = 'images/sun_3104611.png'
+    }else if (data.weather[0].main === 'Drizzle') {
+        weatherIcon.src = 'images/drizzle.png'
+    }else if (data.weather[0].main === 'Rain') {
+        weatherIcon.src = 'images/rain.png'
+    }else if (data.weather[0].main === 'Mist') {
+        weatherIcon.src = 'images/mist_13882572.png'
+    }
 }
 
 searchBtn.addEventListener('click', () =>{
